19ba15caeb1a509b80550d62679096b1252e6ee7
Robel <robel4cs@gmail.com>
2026-06-21 02:35:28 +0300

    developing ligh mode

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -425,6 +425,7 @@ export default function Dashboard() {
             <AccountSwitcher
               mobile
               compact
+              showBalance={false}
               showSelectedNameOnMobile
               onSwitch={(acc) => {
                 setActiveAccount(acc)
